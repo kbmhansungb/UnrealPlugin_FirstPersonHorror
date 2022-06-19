e586321588f0eb3d52570fb800367e78c18e11a2
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2022-06-19 15:28:22 +0900

    StepResponseDataAsset을 추가함

---
+++ Plugins/FirstPerson_Horror/Source/FirstPerson_Horror/Public/StepResponseDataAsset.h
@@ -0,0 +1,17 @@
+// Bummoo Kim, 2022, Free license
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "StepResponseDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPERSON_HORROR_API UStepResponseDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
