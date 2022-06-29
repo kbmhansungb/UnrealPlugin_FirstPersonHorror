4828b89e05e41113b6a154c5253de994108bdf78
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2022-06-29 22:03:04 +0900

    OnLanded시 CallFootStrike함

--- FirstPerson_Horror/Source/FirstPerson_Horror/Public/HorrorPlayerCharacter.h
+++ FirstPerson_Horror/Source/FirstPerson_Horror/Public/HorrorPlayerCharacter.h
@@ -26,8 +26,7 @@ protected:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
+	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
